a7134f92003639f2ae88ecbe558cb3cffaa98507
Edmine <edmine.de@gmail.com>
2024-10-14 16:48:03 -0400

    created table, created func to loop over rows in tables in  order to insert values from tasks

--- todo.py
+++ todo.py
@@ -10,22 +10,54 @@ app = typer.Typer()
 
 # defined add func, and created task, category, and description variables.
 @app.command(short_help='adds an item, enter Oct dd-yyyy HH:mmAM/PM ')
-def add(task: str, category:str, description:str, date_assigned:str):
+def add(task: str, category:str, description:str, date_assigned:str, priority:str):
     format = '%b %d %Y %I:%M%p'
     date_assigned = datetime.datetime.strptime(date_assigned, format)
-    print(f"adding {task}, {category}, {description} , {date_assigned}")
+    print(f"adding {task}, {category}, {description} , {date_assigned}, {priority}")
 
 @app.command()
 def delete(position:int):
     print(f"deleting {position}")
 
 @app.command()
-def update(position:int, task: Optional[str] = None, category: Optional[str] = None, description: Optional[str] = None):
+def update(position:int, task: Optional[str] = None, category: Optional[str] = None, description: Optional[str] = None, 
+date_assigned: Optional[str] = None, priority: Optional[str] = None       ):
     print(f"updating {position}")
 
 @app.command()
 def complete(position:int):
     print(f"complete {position}")
+
+@app.command()
+def show():
+    tasks = [("Apply to Apprenticeship", "LinkedIn", "Fill Out The Application by 10/15", "Oct 09 2024 9:00AM", "HIGH")]
+    console.print("[bold magenta]Todos!", "💻")
+
+    table = Table(show_header=True, header_style="bold_blue")
+    table.add_column("#", style="dim", width=6)
+    table.add_column("Task", width=6, justify="center")
+    table.add_column("Category", width=6, justify="center")
+    table.add_column("Description", width=6, justify="center")
+    table.add_column("Date Assigned", width=6, justify="center")
+    table.add_column("Priority", width=6, justify="center")
+    table.add_column("Complete", width=6, justify="center")
+
+    def get_priority_color(priority):
+        COLORS = {"HIGH": "red", "MEDIUM": "dark_orange", "LOW": "yellow1"}
+        if priority in COLORS:
+            return COLORS[priority]
+        else:
+            return "white"
+
+    for idx, task in enumerate(tasks,start=0):
+        c = get_priority_color(task[-1])
+        is_done_str = "✅" if True ==2 else "❌"
+        table.add_row(str(idx), task[0], task[1], task[2], task[3], f'[{c}]{task[4]}[/{c}]', is_done_str)
+    
+    console.print(table)
+
+
+
 
 
 
